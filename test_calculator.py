ccb89de551e63ed25375ce2e1535bb19ac5c10ea
KhomynZunoviy <145324053+KhomynZunoviy@users.noreply.github.com>
2024-03-19 11:23:01 +0200

    Зміни

--- test_calculator.py
+++ test_calculator.py
@@ -4,6 +4,6 @@ class TestCalculator(unittest.TestCase):
  def test_add(self):
     self.assertEqual(add(3, 4), 7)
  def test_subtract(self):
-    self.assertEqual(subtract(5, 2), 3)
+    self.assertEqual(subtract(5, 4), 1)
 if __name__ == '__main__':
  unittest.main()
